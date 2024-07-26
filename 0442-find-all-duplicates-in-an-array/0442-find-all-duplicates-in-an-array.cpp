e3d3cfd3eb6eeea7662f4261a7b7d7abe81a6062
bhoomikagupta30 <bhoomika2003gupta@gmail.com>
2024-07-26 20:25:57 +0530

    Time: 112 ms (7.07%), Space: 50.4 MB (5.18%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -13,18 +13,22 @@ public:
         int b[m.size()];
 
         map<int,int>:: iterator it;
-        i=0;
+        // i=0;
         for(it=m.begin();it!=m.end();it++){
-            a[i]=it->first;
-            b[i]=it->second;
-            i++;
-        }
+            // a[i]=it->first;
+            // b[i]=it->second;
+            // i++;
 
-        for(i=0;i<m.size();i++){
-            if(b[i]==2){
-                v1.push_back(a[i]);
+            if(it->second==2){
+                v1.push_back(it->first);
             }
         }
+
+        // for(i=0;i<m.size();i++){
+        //     if(b[i]==2){
+        //         v1.push_back(a[i]);
+        //     }
+        // }
 
         return v1;
     }
