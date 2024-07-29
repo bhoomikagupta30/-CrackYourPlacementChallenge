c050bafa99181a148bd24404304840934f4df922
bhoomikagupta30 <bhoomika2003gupta@gmail.com>
2024-07-29 23:33:39 +0530

    Time: 27 ms (59.97%), Space: 16.2 MB (33.54%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,43 +1,6 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        // Time Limit Exceeded O(n^4)
-
-        // int i,j,k,l,sum=0;
-        // set<vector<int>>s1;
-        // vector<int>v1;
-        // vector<vector<int>>ans;
-
-        // sort(nums.begin(),nums.end());
-
-        // for(i=0;i<nums.size();i++){
-        //     sum=0;
-
-        //     for(j=i+1;j<nums.size();j++){
-
-        //         for(k=j+1;k<nums.size();k++){
-
-        //             for(l=k+1;l<nums.size();l++){
-        //                 vector<int>v1;
-        //                 if((long)nums[i]+(long)nums[j]+(long)nums[k]+(long)nums[l]==target){
-        //                     v1.push_back(nums[i]);
-        //                     v1.push_back(nums[j]);
-        //                     v1.push_back(nums[k]);
-        //                     v1.push_back(nums[l]);
-        //                     s1.insert(v1);
-        //                 }
-        //             }
-        //         }
-        //     }
-        // }
-        
-        // set<vector<int>>::iterator it;
-        // for(it=s1.begin();it!=s1.end();it++){
-        //      ans.push_back(*it);
-        // }
-
-        // return ans;
-
         int a,b,c,d,i,j;
         vector<vector<int>>ans;
         sort(nums.begin(),nums.end());
