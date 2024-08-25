ed7c5cc0640e0183e65c41c19dde9a67d06155fc
bhoomikagupta30 <bhoomika2003gupta@gmail.com>
2024-08-25 11:51:27 +0530

    Time: 0 ms (100%), Space: 12.3 MB (48.16%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,28 +1,64 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int i,c=0;
+        // int i,c=0;
 
-        for(i=0;i<nums.size();i++){
-            if(nums[i]==target){
-                c=1;
-                break;
+        // for(i=0;i<nums.size();i++){
+        //     if(nums[i]==target){
+        //         c=1;
+        //         break;
+        //     }
+        // }
+
+        // if(c==1){
+        //     return i;
+        // }
+        // else{
+        // int j=0;
+        // sort(nums.begin(),nums.end());
+
+        // while(j<nums.size() && target>nums[j]){
+        //     j++;
+        // }
+
+        // return j;
+
+        // }
+
+        int low=0;
+        int high=nums.size()-1;
+        int mid;
+
+        
+
+        while(low<=high){
+            mid=(low+high)/2;
+
+            if(target>nums[mid]){
+                low=mid+1;
+            }
+            else{
+                if(target<nums[mid]){
+                    high=mid-1;
+                }
+                else{
+                    return mid;
+                }
             }
         }
 
-        if(c==1){
-            return i;
-        }
-        else{
-        int j=0;
-        sort(nums.begin(),nums.end());
+        return low;
 
-        while(j<nums.size() && target>nums[j]){
-            j++;
-        }
 
-        return j;
 
-        }
+
+
+
+
+
+
+
+
+
     }
 };
