93a5e9130e98aec541de9920d6a30ae800d80349
bhoomikagupta30 <bhoomika2003gupta@gmail.com>
2024-11-05 16:58:10 +0530

    Time: 16 ms (11.1%), Space: 15.2 MB (6.58%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,28 +9,64 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode *temp=head;
+    //     ListNode *temp=head;
 
-        map<ListNode,int>m1;
+    //    if(temp!=NULL){
+    //     while(1){
+    //         if(temp->next==NULL){
+    //             return false;
+    //         }
+    //         else{
 
-        if(temp!=NULL){
-        while(1){
-            if(temp->next==NULL){
-                return false;
-            }
-            else{
+    //             if(temp->next<temp || temp->next==temp){
+    //                 return true;
+    //             }
+    //         }
 
-                if(temp->next<temp || temp->next==temp){
-                    return true;
-                }
-            }
+    //         temp=temp->next;
+    //     }
 
-            temp=temp->next;
-        }
+    //     }
+    //     else{
+    //         return false;
+    //     }
+    // }
 
-        }
-        else{
-            return false;
-        }
+    map<ListNode*,int>m;
+
+    ListNode *temp=head;
+
+    if(temp!=NULL){
+    while(1){
+
+      if(m.find(temp)!=m.end()){
+         return true;
+      }
+      else{
+         m[temp]=temp->val;
+      }
+
+      if(temp->next!=NULL){
+      temp=temp->next;
+      }
+      else{
+        break;
+      }
+
+
+
+
+
+
+
+
     }
+
+    }
+    return false;
+
+    
+
+    }
+
 };
