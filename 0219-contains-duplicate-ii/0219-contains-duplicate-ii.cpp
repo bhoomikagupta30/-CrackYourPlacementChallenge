92d64ec93d0bbe7203712f1aead8c54394c7d3a9
bhoomikagupta30 <bhoomika2003gupta@gmail.com>
2024-11-10 23:52:36 +0530

    Time: 1774 ms (13.78%), Space: 62.9 MB (96.49%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,24 +1,42 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        int i,j;
+        // int i,j;
 
-        map<int,int>m;
+        // map<int,int>m;
 
-        for(i=0;i<nums.size();i++){
-            if(m.find(nums[i])==m.end()){
-                m[nums[i]]=i;
-            }
-            else{
-               if(abs(m[nums[i]]-i)<=k){
-                   return true;
-               }
-               else{
-                  m[nums[i]]=i;
-               }
-            }
+        // for(i=0;i<nums.size();i++){
+        //     if(m.find(nums[i])==m.end()){
+        //         m[nums[i]]=i;
+        //     }
+        //     else{
+        //        if(abs(m[nums[i]]-i)<=k){
+        //            return true;
+        //        }
+        //        else{
+        //           m[nums[i]]=i;
+        //        }
+        //     }
+        // }
+
+        // return false;
+
+        int n=nums.size();
+        int i=0;
+        if(k==0){
+            return false;
         }
-
+        while(i<n){
+            int j=i+1;
+            while(j<=i+k&&j<n){
+            if(nums[i]==nums[j]){
+                return true;
+            }
+                j++;
+            }
+           i++;
+        }
+    
         return false;
 
         
