b1d0fd8d8c973a13f5555740fa738d820eed133c
bhoomikagupta30 <bhoomika2003gupta@gmail.com>
2024-12-14 17:00:16 +0530

    Time: 35 ms (31.08%), Space: 65.1 MB (42.31%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -24,41 +24,61 @@ public:
 
         // return k;
 
+        //Time--> O(N) Space--> O(N)
+        // map<int,int>m1;
 
-        map<int,int>m1;
+        // int i;
 
-        int i;
+        // for(int i=0;i<nums.size();i++){
+        //     m1[nums[i]]++;
+        // }
 
-        for(int i=0;i<nums.size();i++){
-            m1[nums[i]]++;
-        }
+        // int l;
+        // map<int,int>::iterator it;
+        // for(it=m1.begin();it!=m1.end();it++){
+        //     if(it->second>1){
+        //        l=it->first;
+        //        break;
+        //     }
+        // }
 
-        int l;
-        map<int,int>::iterator it;
-        for(it=m1.begin();it!=m1.end();it++){
-            if(it->second>1){
-               l=it->first;
-               break;
+
+        // return l;
+
+
+        sort(nums.begin(),nums.end());
+
+        int i=0;
+        int j=1;
+        int k;
+
+        while(j<nums.size()){
+            if(nums[i]==nums[j]){
+                k=nums[i];
+                break;
+            }
+            else{
+                i++;
+                j++;
             }
         }
 
-
-        return l;
-
-
-
-
-
-
-
-
-
-
-
+        
+      return k;
 
 
 
     }
+
+
+
+
+
+
+
+
+
+    
 
 
     };
