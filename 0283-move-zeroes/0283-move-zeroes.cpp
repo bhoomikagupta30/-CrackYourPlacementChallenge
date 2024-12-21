e6b86d8f6210e545bec883734216cade2934ccb6
bhoomikagupta30 <bhoomika2003gupta@gmail.com>
2024-12-21 13:22:33 +0530

    Time: 0 ms (100%), Space: 23.9 MB (20.42%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,29 +1,44 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        vector<int>v1;
+        // vector<int>v1;
+
+        // int i,j;
+
+        // for(i=0;i<nums.size();i++){
+        //     if(nums[i]!=0){
+        //         v1.push_back(nums[i]);
+        //     }
+        // }
+
+        // int n=nums.size();
+        // int k=v1.size();
+
+        // if(k<n){
+
+        //     while(v1.size()<n){
+        //         v1.push_back(0);
+
+        //     }
+        // }
+
+        // for(i=0;i<nums.size();i++){
+        //     nums[i]=v1[i];
+        // }
+
+        int k=0;
 
         int i,j;
 
         for(i=0;i<nums.size();i++){
             if(nums[i]!=0){
-                v1.push_back(nums[i]);
+                nums[k]=nums[i];
+                k++;
             }
         }
 
-        int n=nums.size();
-        int k=v1.size();
-
-        if(k<n){
-
-            while(v1.size()<n){
-                v1.push_back(0);
-
-            }
-        }
-
-        for(i=0;i<nums.size();i++){
-            nums[i]=v1[i];
+        for(i=k;i<nums.size();i++){
+            nums[i]=0;
         }
     }
 };
